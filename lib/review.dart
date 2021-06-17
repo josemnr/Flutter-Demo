2d2b753960fcda3a12894a6382896d03a6324fad
josemnr <maninari97@gmail.com>
2021-06-17 12:59:00 -0500

    init repo

---
+++ lib/review.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class Review extends StatelessWidget {
+  String pathImage = "assets/img/people.jpeg";
+  String name = "Varuna Yasas";
+  String details = "1 review 5 photos";
+  String comment = "There is an amazing place in Sri Lanka";
+
+  Review(this.pathImage, this.name, this.details, this.comment);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    final userComment = Container(
+      margin: EdgeInsets.only(left: 20),
+      child: Text(
+        comment,
+        textAlign: TextAlign.left,
+        style: TextStyle(
+            fontFamily: "Lato", fontSize: 13, fontWeight: FontWeight.w900),
+      ),
+    );
+
+    final userInfo = Container(
+      margin: EdgeInsets.only(left: 20),
+      child: Text(
+        details,
+        textAlign: TextAlign.left,
+        style: TextStyle(
+            fontFamily: "Lato", fontSize: 13, color: Color(0xFFa3a5a7)),
+      ),
+    );
+
+    final userName = Container(
+      margin: EdgeInsets.only(left: 20),
+      child: Text(
+        name,
+        textAlign: TextAlign.left,
+        style: TextStyle(fontFamily: "Lato", fontSize: 17),
+      ),
+    );
+
+    final userDetails = Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [userName, userInfo, userComment],
+    );
+
+    final photo = Container(
+      margin: EdgeInsets.only(
+        top: 20,
+        left: 20,
+      ),
+      width: 80,
+      height: 80,
+      decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          image:
+              DecorationImage(fit: BoxFit.cover, image: AssetImage(pathImage))),
+    );
+
+    return Row(
+      children: [photo, userDetails],
+    );
+  }
+}
---
+++ lib/review_list.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'review.dart';
+
+class ReviewList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Review("assets/img/people.jpeg", "Varuna Yasas", "1 review · 5 photos",
+            "There is an amazing place in Sri Lanka"),
+        Review("assets/img/pepe.jpg", "José Navarro", "2 review · 5 photos",
+            "There is an amazing place in Sri Lanka"),
+        Review("assets/img/girl.jpeg", "Gissele Thomas", "2 review · 2 photos",
+            "There is an amazing place in Sri Lanka"),
+      ],
+    );
+  }
+}
